427f4408525db9ca9ea89ee6b63fd6f15534e141
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2020-11-16 17:41:52 +0100

    Add files via upload

--- eagletrt-ubx-parser-master/main.c
+++ eagletrt-ubx-parser-master/main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <string.h>
 #include<json-c/json.h>
+#include <wjelement.h>
+#include <wjreader.h>
 
 #include "utils/config_parser/config_parser.h"
 #include "utils/config_parser/config_parser.c"
@@ -10,19 +12,29 @@
 #include "utils/parse_ubx/parse_ubx.c"
 #include "utils/generate_output/generate_output.h"
 #include "utils/generate_output/generate_output.c"
+#include "utils/validate_config/validate_config.h"
+#include "utils/validate_config/validate_config.c"
+#include "utils/log_utils/log_utils.h"
+#include "utils/log_utils/log_utils.c"
+#include "utils/log_utils/log_utils_colors.h"
 
 int main(){
 	message_t messages[2000];
 	config_t path;
-	int i;
-		
+	int i,validate;
+	
+	validate=validateConfig();
+	if (validate){
+		logError("Invalide config.json");
+		return 1;
+	}	
 	path=parse_config();
 	for(i=0;i<path.inputs_count;i++){
 		printf("%d %s\n",i+1, path.path[i]);
-		parse_ubx(path.path[i], messages);
+		parse_ubx(path.path[i], messages, path.limit_warning);
 		generate_output(path.path[i],messages,path.type);
 	}		
-	printf("Fine scansione\n");
+	logSuccess("Fine scansione\n");
 	
 }
 
